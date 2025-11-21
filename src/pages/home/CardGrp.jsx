4fa04cfa1606191ab20914a5afbd788f4cfcefa5
Balaraman-Dev <balaramann2004@gmail.com>
2025-11-22 00:33:55 +0530

    home page width

--- src/pages/home/CardGrp.jsx
+++ src/pages/home/CardGrp.jsx
@@ -4,7 +4,7 @@ import Card from "@/components/Card";
 
 const CardGrp = ( { tours } ) => {
     return (    
-        <div className="flex md:-my-12 -my-4  sm:-ml-20 gap-2 sm:scale-[85%] scale-60 justify-center items-center">
+        <div className="flex md:-my-12 -my-4  sm:-ml-20 gap-2 sm:scale-[70%] scale-80 justify-center items-center">
 
             <Card tour={tours[1]} className="w-4/12 z-10 -rotate-[10deg] scale-[85%] hover:z-50 hover:-rotate-[5deg] hover:scale-100 origin-bottom-right"></Card>
             <Card tour={tours[0]} className="w-4/12 z-20 hover:z-50 hover:scale-105"></Card>
